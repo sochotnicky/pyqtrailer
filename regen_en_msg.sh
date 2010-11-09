873c8d4f0795e4a71941256f722934c19d194c58
Stanislav Ochotnicky <sochotnicky@redhat.com>
2010-11-09 14:02:17 +0100

    Move original english messages to messages.po

--- regen_en_msg.sh
+++ regen_en_msg.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 pylupdate4 `find pyqtrailer/ -name '*.py'` -ts temp.ts
 
-lconvert --source-language en_GB -i temp.ts --target-language en_GB -o po/en_GB.po
+lconvert --source-language en_GB -i temp.ts --target-language en_GB -o po/messages.po
